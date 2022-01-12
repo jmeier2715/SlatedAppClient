6cfcbde25157ca2b64a3716fc0cc15d71a17c961
jmeier2715 <jmeier2715@gmail.com>
2022-01-11 20:01:23 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+const Home = (props) => {
+	// const { msgAlert, user } = props
+	console.log('props in home', props)
+
+	return (
+		<>
+			<h2>Bing bang boom</h2>
+		</>
+	)
+}
+
+export default Home
